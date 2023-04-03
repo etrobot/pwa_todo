a484b75bece38fdd898eb5f7cbf52103cb37f690
franklin <etrobot@outlook.com>
2023-04-04 00:10:17 +0800

    get conversations

--- src/components/header.ts
+++ src/components/header.ts
@@ -8,6 +8,7 @@ import '@shoelace-style/shoelace/dist/components/drawer/drawer.js';
 export class AppHeader extends LitElement {
   @property({ type: String }) title = 'PWA Starter';
   @property({ type: Boolean }) drawerOpen = false;
+  @property({ type: Array }) conversations = [];
 
   static get styles() {
     return css`
@@ -19,27 +20,60 @@ export class AppHeader extends LitElement {
         position: fixed;
         left: env(titlebar-area-x, 0);
         top: env(titlebar-area-y, 0);
-        height: env(titlebar-area-height, 50px);
-        width: env(titlebar-area-width, 100%);
-        -webkit-app-region: drag;
       }
 
+      ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+      }
+
+      li {
+        padding: 0.5em;
+        cursor: pointer;
+      }
+
+      li:hover {
+        background: var(--sl-color-neutral-100);
+      }
     `;
+  }
+
+  async connectedCallback() {
+    super.connectedCallback();
+    const token = localStorage.getItem('token');
+    const response = await fetch('http://localhost:8080/api/conversations', {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
+    if (response.ok) {
+      const conversations = await response.json();
+      this.conversations = conversations.map(([id, title]:[Number,String]) => ({ id, title }));
+    }
   }
 
   render() {
     return html`
       <header>
-        <sl-icon-button name="list" @click="${this.toggleDrawer}">></sl-icon-button>
+        <sl-button name="list" @click="${this.toggleDrawer}">></sl-button>
         <span>${this.title}</span>
       </header>
       <sl-drawer label="Conversations"  placement="start" ?open="${this.drawerOpen}">
-      This drawer slides in from the start.
+        <ul>
+          ${this.conversations.map(({ id, title }) => html`
+            <li @click="${() => this.selectConversation(id)}">${title}</li>
+          `)}
+        </ul>
       </sl-drawer>
     `;
   }
 
   toggleDrawer() {
     this.drawerOpen = !this.drawerOpen;
+  }
+
+  selectConversation(id:Number) {
+    console.log(`Selected conversation ${id}`);
   }
 }
